4691850972eaa072b1647048d1412bf2fabcac4d
maria-studiodialect <maria@studiodialect.com>
2024-03-28 15:56:53 +0000

    seo

--- pages/_document.js
+++ pages/_document.js
@@ -6,13 +6,13 @@ export default function Document() {
       <Head>
 
         <meta name="title" content="Studio Dialect"/>
-        <meta name="description" content="Simon Hathaway Joins Studio Dialect"/>
+        <meta name="description" content="Studio Dialect is thrilled to announce the appointment of Simon Hathaway as Non-Executive Chairman, effective immediately."/>
 
         {/* Open Graph / Facebook */}
         <meta property="og:type" content="website"/>
         <meta property="og:url" content="https://pr.studiodialect.com/"/>
         <meta property="og:title" content="Studio Dialect"/>
-        <meta property="og:description" content="Simon Hathaway Joins Studio Dialect"/>
+        <meta property="og:description" content="Studio Dialect is thrilled to announce the appointment of Simon Hathaway as Non-Executive Chairman, effective immediately."/>
         <meta property="og:image" content="/cards.jpg"/>
       </Head>
       <body>
