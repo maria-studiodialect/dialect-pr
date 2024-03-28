34365b08261f5e1b57792ccf6367b0d99873f8ab
maria-studiodialect <maria@studiodialect.com>
2024-03-28 15:29:17 +0000

    seo

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,18 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+
+        <meta name="title" content="Studio Dialect PR"/>
+        <meta name="description" content="Simon Hathaway Joins Studio Dialect"/>
+
+        {/* Open Graph / Facebook */}
+        <meta property="og:type" content="website"/>
+        <meta property="og:url" content="https://pr.studiodialect.com/"/>
+        <meta property="og:title" content="Studio Dialect PR"/>
+        <meta property="og:description" content="Simon Hathaway Joins Studio Dialect"/>
+        <meta property="og:image" content="/cards-size.jpg"/>
+      </Head>
       <body>
         <Main />
         <NextScript />
